ae92fbd5d9860064fb9b43756dc67df88617399b
wintercameearly <oluwanifemi.bamgbose@stu.cu.edu.ng>
2020-08-25 23:40:39 +0100

    Fixed again

--- internshiptest/settings.py
+++ internshiptest/settings.py
@@ -123,7 +123,7 @@ STATICFILES_DIRS = (
 )
 
 #  Add configuration for static files storage using whitenoise
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.django.CompressedManifestStaticFilesStorage'
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
